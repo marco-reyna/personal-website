e24b5517ea8a9de19cdd5b069c1afd7ef6bb864b
Marco <marcoreynarinaldi@gmail.com>
2023-02-17 01:05:40 +0100

    built parallax effect/section five

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import SectionTwo from './components/Section02/SectionTwo';
 import SectionThree from './components/Section03/SectionThree';
 import SectionFour from './components/Section04/SectionFour';
 import SectionFive from './components/Section05/SectionFive';
+// import SectionParallax from './components/Section05/SectionParallax';
+import SectionContact from './components/SectionContact/SectionContact';
 
 function App() {
 
@@ -14,6 +16,8 @@ function App() {
       <SectionThree />
       <SectionFour />
       <SectionFive />
+      {/* <SectionParallax /> */}
+      <SectionContact />
     </>
   )
 }
---
+++ src/components/SectionContact/SectionContact.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import classes from './SectionContact.module.css';
+
+function SectionContact() {
+
+  return (
+    <div className={classes.container}>
+      <div className={classes.box}>
+        <p className={classes.text}>Get in touch</p>
+      </div>
+    </div>
+  )
+}
+
+export default SectionContact;
