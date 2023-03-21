f7580b9ee2ac25e36b9152220ae632202760f85c
Marco <marcoreynarinaldi@gmail.com>
2023-03-21 01:20:39 +0100

    added animations to section one and hovering

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import ProgressBar from './components/ProgressBar';
-import SideBar from './components/SideBar/SideBar';
-import SectionOne from './components/Section01/SectionOne';
-import SectionTwo from './components/Section02/SectionTwo';
-import SectionThree from './components/Section03/SectionThree';
-import SectionFour from './components/Section04/SectionFour';
-import SectionFive from './components/Section05/SectionFive';
-import SectionSix from './components/Section06/SectionSix';
-import SectionContact from './components/SectionContact/SectionContact';
-import Footer from './components/Footer';
-import useScrollingProgress from './hooks/useScrollingProgress';
+import React from "react";
+import ProgressBar from "./components/ProgressBar";
+import SideBar from "./components/SideBar/SideBar";
+import SectionOne from "./components/Section01/SectionOne";
+import SectionTwo from "./components/Section02/SectionTwo";
+import SectionThree from "./components/Section03/SectionThree";
+import SectionFour from "./components/Section04/SectionFour";
+import SectionFive from "./components/Section05/SectionFive";
+import SectionSix from "./components/Section06/SectionSix";
+import SectionContact from "./components/SectionContact/SectionContact";
+import Footer from "./components/Footer";
+import useScrollingProgress from "./hooks/useScrollingProgress";
 
 function App() {
   const scrolling = useScrollingProgress();
 
   return (
     <>
-      <ProgressBar scrolling={scrolling + '%'}/>
+      <ProgressBar scrolling={scrolling + "%"} />
       <SideBar scrolling={scrolling} />
-      <SectionOne />
+      <SectionOne scrolling={scrolling} />
       <SectionTwo />
       <SectionThree />
       <SectionFour />
@@ -27,7 +27,7 @@ function App() {
       <SectionContact />
       <Footer />
     </>
-  )
+  );
 }
 
 export default App;
--- src/components/Section01/SectionOne.jsx
+++ src/components/Section01/SectionOne.jsx
@@ -1,25 +1,36 @@
-import React from 'react';
-import classes from './SectionOne.module.css';
-import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
-import Zoom from '@mui/material/Zoom';
+import React from "react";
+import classes from "./SectionOne.module.css";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Zoom from "@mui/material/Zoom";
+import { useInView } from "react-intersection-observer";
 
-function SectionOne() {
+function SectionOne(props) {
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+  });
 
   return (
     <section className={classes.container} id="section-1">
-        <div className={classes.nameBox}>
-          <div>Hi, I'm</div>
-          <div className={classes.firstName}>Marco</div>
-          <div className={classes.lastName}>Reyna</div>
-          <div className={classes.position}>Frontend Developer</div>
-        </div>
-        <div className={classes.icon}>
-          <Zoom in={true}>
-            <KeyboardArrowDownIcon/>
-          </Zoom>
-        </div>
+      <div
+        className={`${classes.nameBox} ${inView ? classes.showNameBox : ""}`}
+        ref={ref}
+      >
+        <div>Hi, I'm</div>
+        <div className={classes.firstName}>Marco</div>
+        <div className={classes.lastName}>Reyna</div>
+        <div className={classes.position}>Frontend Developer</div>
+      </div>
+      <div
+        className={`${classes.icon} ${
+          props.scrolling > 2 ? classes.hideIcon : classes.showIcon
+        }`}
+      >
+        <Zoom in={true}>
+          <KeyboardArrowDownIcon />
+        </Zoom>
+      </div>
     </section>
-  )
+  );
 }
 
 export default SectionOne;
